39e384311af5135ae42890460078a488773d06b0
punitsoni22 <punitsoni14476@gmail.com>
2023-07-26 14:39:03 +0530

    ok

--- Level_2/Task_3/todo.js
+++ Level_2/Task_3/todo.js
@@ -12,10 +12,37 @@ function addTask(){
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+
+        // let em = document.createElement("em");
+        // em.innerHTML = "EDIT";
+        // li.appendChild(em);
         
+        let task_edit = document.createElement("button");
+        task_edit.classList.add('edit');
+        task_edit.innerText = 'Edit';
+        li.appendChild(task_edit);
     }
     inputList.value ="";
 }
+
+const task_input = document.createElement('input');
+task_input.classList.add('text');
+task_input,type = 'text';
+task_input.value = "task";
+task_input.setAttribute('readonly', 'readonly');
+task_input.appendChild(task_input);
+
+task_edit.addEventListener('click', (e) => {
+    if(task_edit.innerText.toLowercase() === "edit"){
+        task_edit.innerText = 'Save';
+        task_input.removeAttribute("readonly");
+        task_input.focus();
+    } else{
+        task_edit.innerText = 'Edit';
+        task_input.setAttribute("readonly", "readonly");
+    }
+})
+
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle("checked");
@@ -23,7 +50,5 @@ listContainer.addEventListener("click", function(e){
     else if(e.target.tagName === 'SPAN'){
         e.target.parentElement.remove();
     }
-    else if(e.target.tagName === 'SPANN'){
-        e.target.parentElement.edit();
-    }
 });
+
