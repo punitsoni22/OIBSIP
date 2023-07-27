48df0a60afb7f2ce26ffe7305478f472512dd03f
punitsoni22 <punitsoni14476@gmail.com>
2023-07-27 10:47:03 +0530

    todo done

--- Level_2/Task_3/todo.js
+++ Level_2/Task_3/todo.js
@@ -1,37 +1,68 @@
-const inputList = document.getElementById("inp-list");
-const listContainer = document.getElementById("task-list");
+const addUserBtn = document.getElementById('addUser');
+const btnText = addUserBtn.innerText;
+const usernameTextField = document.getElementById('username');
+const recordsDisplay = document.getElementById('records');
+let userArray = [];
+let edit_id = null;
 
-function addTask(){
-    if(inputList.value == ''){
-        alert("You Must Write Something!")
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputList.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+let objStr = localStorage.getItem('users');
 
-        // let em = document.createElement("em");
-        // em.innerHTML = "EDIT";
-        // li.appendChild(em);
-        
-        let task_edit = document.createElement("button");
-        task_edit.classList.add('edit');
-        task_edit.innerText = 'Edit';
-        li.appendChild(task_edit);
-    }
-    inputList.value ="";
+if (objStr != null) {
+   userArray = JSON.parse(objStr);
 }
 
+DisplayInfo();
+addUserBtn.onclick = () => {
+   //get user's name from text field
+   const name = usernameTextField.value;
+   if (edit_id != null) {
+      //edit action
+      userArray.splice(edit_id, 1, {
+         'name': name
+      });
+      edit_id = null;
+   } else {
+      //insert action
+      userArray.push({
+         'name': name
+      });
+   }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === 'LI'){
-        e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagName === 'SPAN'){
-        e.target.parentElement.remove();
-    }
-});
+   SaveInfo(userArray);
+   usernameTextField.value = '';
+   addUserBtn.innerText = btnText;
+}
 
+// store user's name in local storage
+function SaveInfo(userArray) {
+   let str = JSON.stringify(userArray);
+   localStorage.setItem('users', str);
+   DisplayInfo();
+}
+
+// display user's name
+function DisplayInfo() {
+   let statement = '';
+   userArray.forEach((user, i) => {
+      statement += `<tr>
+           <th scope="row">${i+1}</th>
+           <td>${user.name}</td>
+           <td><i class="btn text-white fa fa-edit btn-info mx-2" onclick='EditInfo(${i})'></i> <i class="btn btn-danger text-white fa fa-trash" onclick='DeleteInfo(${i})'></i></td>
+         </tr>`;
+   });
+   recordsDisplay.innerHTML = statement;
+}
+
+// edit user's name
+function EditInfo(id) {
+   edit_id = id;
+   usernameTextField.value = userArray[id].name;
+   addUserBtn.innerText = 'Save Changes';
+}
+
+//delete user's name
+function DeleteInfo(id) {
+   userArray.splice(id, 1);
+   SaveInfo(userArray);
+
+}
